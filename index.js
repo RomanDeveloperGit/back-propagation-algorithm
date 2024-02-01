20267c39b70a9a4747bfbc4d9c6b8c1bcd4f48f9
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2024-02-01 08:44:36 +0300

    feat(*): add init weights function

--- index.js
+++ index.js
@@ -15,10 +15,35 @@ const hiddenLayerNeuronWeights = [];
 
 // Сигмоидальная функция активации для скрытых слоев
 const getProcessedValueByHiddenActivation = (value) => {
-  return 1 / (1 + Math.exp(-value))
+  return 1 / (1 + Math.exp(-value));
 };
 
 // Линейная функция активации для выходного слоя
 const getProcessedValueByOutputActivation = (value) => {
   return value;
 };
+
+// Далее для обучения код:
+
+const getRandomNumber = () => {
+  return Math.random().toFixed(3);
+};
+
+const initWeights = () => {
+  for (
+    let hiddenLayer = 0;
+    hiddenLayer < hiddenLayerNeuronCount;
+    hiddenLayer++
+  ) {
+    for (let neuron = 0; neuron < inputCount; neuron++) {
+      if (!Array.isArray(hiddenLayerNeuronWeights[hiddenLayer]))
+        hiddenLayerNeuronWeights[hiddenLayer] = [];
+
+      hiddenLayerNeuronWeights[hiddenLayer][neuron] = getRandomNumber();
+    }
+  }
+};
+
+console.log(hiddenLayerNeuronWeights);
+initWeights();
+console.log(hiddenLayerNeuronWeights);
